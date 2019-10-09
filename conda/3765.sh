78ff7519a209232b9e7e36b323d46ff72e1317e4
Julian Rüth <julian.rueth@fsfe.org>
2019-10-09 05:08:45 +0200

    copy some scripts over to the correct workdir

--- conda/3765.sh
+++ conda/3765.sh
@@ -3,6 +3,7 @@
 # ##########################################################
 
 mv $SRC_DIR/work $SRC_DIR/../real_work
+mv $SRC_DIR/* $SRC_DIR/../real_work/
 rmdir $SRC_DIR
 mv $SRC_DIR/../real_work $SRC_DIR
 cd `pwd`
