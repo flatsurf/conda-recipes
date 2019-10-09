db901636b7e93c0853e386ec212cb88b77a47218
Julian Rüth <julian.rueth@fsfe.org>
2019-10-09 04:46:00 +0200

    work around conda-build bug

---
+++ conda/3765.sh
@@ -0,0 +1,8 @@
+# ##########################################################
+# Work around https://github.com/conda/conda-build/pull/3765
+# ##########################################################
+
+mv $SRC_DIR/work $SRC_DIR/../real_work
+rmdir $SRC_DIR
+mv $SRC_DIR/../real_work $SRC_DIR
+cd `pwd`
