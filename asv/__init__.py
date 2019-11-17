20cf53f51701650ea227dd115377db500a573487
Julian Rüth <julian.rueth@fsfe.org>
2019-11-17 19:58:47 +0100

    Adapt to standalone benchmark
    benchmarks used to be mixed with tests. Now they are on their own.
    
    This does not yet allow to run benchmarks individually with asv.

--- asv/__init__.py
+++ asv/__init__.py
@@ -38,16 +38,12 @@ BENCHMARKS = {}
 PREFIX = "track_"
 
 os.chdir(ASV_BUILD_DIR)
-for benchmark in glob.glob('**/test/*.*.cc'):
+for benchmark in glob.glob('**/benchmark/'):
     os.system("echo 'Processing %s'"%benchmark)
-    os.chdir(ASV_BUILD_DIR)
-    os.chdir(os.path.dirname(benchmark))
-    benchmark = os.path.basename(benchmark).replace('.', '_')[:-3]
-    if benchmark.endswith("_test"): benchmark = benchmark[:-5]
-    subprocess.check_call(["make", benchmark])
-    subprocess.check_call(["./%s"%(benchmark,), "--benchmark_format=json", "--benchmark_out=%s.json"%(benchmark,), "--gtest_repeat=0"])
-    if os.lstat("%s.json"%(benchmark,)).st_size == 0: continue
-    results = json.load(open("%s.json"%(benchmark,), "r"))
+    os.chdir(benchmark)
+    subprocess.check_call(["make", "benchmark"])
+    subprocess.check_call(["./benchmark", "--benchmark_format=json", "--benchmark_out=benchmark.json"])
+    results = json.load(open("benchmark.json", "r"))
     for bench in results["benchmarks"]:
         name = bench["name"]
         time = float(bench["cpu_time"])
