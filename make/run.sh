8620c706bdd16c07a505121c35029cec10ed47b7
Julian Rüth <julian.rueth@fsfe.org>
2020-06-29 03:45:05 +0200

    fix installation

--- make/run.sh
+++ make/run.sh
@@ -5,30 +5,33 @@ set -exo pipefail
 # Run make, make check and friends
 # ##################################################################
 
-if [[ "$action" == "style" ]]; then
-  exit 0
-fi
-
-# Build everything
-make CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS"
-
 case $action in
-  release|style|benchmark) exit 0;
+  release)
+    make install CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS"
+    exit 0
+    ;;
+  style)
+    exit 0
+    ;;
+  *)
+    # Build everything
+    make CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS"
 esac
 
-# Run all test cases
-make check CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS" || (cat `find . -name 'test-suite*.log'` /dev/null; false)
-
-# Run additional tests (might be too slow on non-release builds.)
-if [[ "$target" == "test" ]]; then
+case $action in
+  test)
+    # Run all test cases
+    make check CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS" || (cat `find . -name 'test-suite*.log'` /dev/null; false)
+    # Run additional tests (might be too slow on non-release builds.)
     for subdir in ${SUBDIRS//:/$IFS}; do
         pushd $subdir
         make check-valgrind || (echo | cat `find . -name 'test-suite*.log'` /dev/null; false)
         popd
     done
     make distcheck || (echo | cat `find . -name test-suite.log` /dev/null; false)
-fi
-
-if [[ $action == "release" ]]; then
-    make install CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS"
-fi
+    ;;
+  coverage)
+    # Run all test cases
+    make check CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS" || (cat `find . -name 'test-suite*.log'` /dev/null; false)
+    ;;
+esac
